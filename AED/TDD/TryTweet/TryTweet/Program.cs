a29fadf20690316e89da5518c83767fc888d4d50
Sunil Pitkar <sunil_pitkar@HOTMAIL.COM>
2013-03-18 10:10:08 -0700

    Last commit from work

---
+++ AED/TDD/TryTweet/TryTweet/Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Configuration;
+using System.Diagnostics;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TweetSharp;
+using TweetSharp.Model;
+
+namespace TryTweet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TwitterClientInfo twitterClientInfo = new TwitterClientInfo(); 
+            twitterClientInfo.ConsumerKey = ConsumerKey; //Read ConsumerKey out of the app.config 
+            twitterClientInfo.ConsumerSecret = ConsumerSecret; //Read the ConsumerSecret out the app.config  
+            TwitterService twitterService = new TwitterService(twitterClientInfo); 
+
+            //Firstly we need the RequestToken and the AuthorisationUrl 
+
+OAuthToken requestToken = twitterService.GetRequestToken();
+//OAuthToken requestToken = Hammock.Authentication.OAuth
+string authUrl = twitterService.GetAuthorizationUrl(requestToken); 
+
+
+
+//authUrl is just a URL we can open IE and paste it in if we want 
+
+Console.WriteLine("Please Allow This App to send Tweets on your behalf"); 
+Process.Start(authUrl); //Launches a browser that'll go to the AuthUrl. 
+
+   
+
+//Allow the App 
+
+Console.WriteLine("Enter the PIN from the Browser:"); 
+
+string pin = Console.ReadLine(); 
+
+  
+
+OAuthToken accessToken = twitterService.GetAccessToken(requestToken, pin); 
+
+  
+
+string token = accessToken.Token; //Attach the Debugger and put a break point here 
+
+string tokenSecret = accessToken.TokenSecret; //And another Breakpoint here 
+
+   
+
+Console.WriteLine("Write Down The AccessToken: " + token); 
+
+Console.WriteLine("Write Down the AccessTokenSecret: " + tokenSecret); 
+
+   
+
+   
+
+   
+
+Console.ReadLine(); 
+
+
+        }
+        #region ConsumerKey & ConsumerSecret 
+
+private static string ConsumerSecret 
+
+{ 
+
+get { return ConfigurationManager.AppSettings["ConsumerSecret"]; } 
+
+} 
+
+   
+
+private static string ConsumerKey 
+
+{ 
+
+get { return ConfigurationManager.AppSettings["ConsumerKey"]; } 
+
+} 
+
+#endregion 
+} 
+
+} 
+
+
+    
+
---
+++ AED/TDD/TryingTinyTwitter/TryingTinyTwitter/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace TryingTinyTwitter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //OAuthInfo myOAuth = new OAuthInfo();
+            //myOAuth.ConsumerKey = "";
+            //myOAuth.ConsumerSecret = "";
+            //myOAuth.AccessSecret = "";
+            //myOAuth.AccessToken = "";
+
+            //var myOauth = new
+
+            //TinyTwitter TT = new TinyTwitter(new OAuthInfo());
+
+            //IEnumerable<Tweet> mytimneline = TT.GetHomeTimeline(null, 2);
+            var oauth = new OAuthInfo
+            {
+                AccessToken = "21985278-dud1wSertHCQYTUK5ta5AA0ciqWB31ZsT8Dt8DJg",
+                AccessSecret = "yPDp2TTOOhQj6XDxX7P5TxmNtHZcQ6sJumth8DVzRk",
+                ConsumerKey = "tedRo766zL7mr7TKZkOugA",
+                ConsumerSecret = "WREOp5SZ71EtLCt3T4RboUv1IrkUpPkCpcBxkAGk8"
+            };
+
+            var twitter = new TinyTwitter(oauth);
+
+            //// Update status, i.e, post a new tweet
+            //twitter.UpdateStatus("I'm tweeting from C#");
+
+            // Get home timeline tweets
+            var tweets = twitter.GetUserTimeline(null, 5);
+
+            //twitter.UpdateStatus("First tweet from my program");
+           // twitter.UpdateStatus("First tweet from my program");
+            foreach (var tweet in tweets)
+                Console.WriteLine("{0}: {1}: {2}", tweet.UserName, tweet.Text, tweet.Id);
+           
+            
+            Console.ReadKey();
+
+
+        }
+    }
+}
